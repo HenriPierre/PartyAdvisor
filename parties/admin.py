09caeb993c1353d66961596e86565ff3e0ae0614
HenriPierre <hptribollet@gmail.com>
2016-07-13 22:15:10 +0200

    First parties

--- parties/admin.py
+++ parties/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# import your model
+from parties.models import Party
+
+# set up automated slug creation
+class PartyAdmin(admin.ModelAdmin):
+	model = Party
+	list_display = ('name', 'description',)
+	prepopulated_fields = {'slug': ('name',)}
+# and register it
+admin.site.register(Party, PartyAdmin)
--- parties/views.py
+++ parties/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from parties.models import Party
 
 # Create your views here.
 def index(request):
 	# this is your new view
-	return render(request, 'index.html')
+	partys = Party.objects.all()
+	return render(request, 'index.html', {
+		'partys':partys,
+	})
